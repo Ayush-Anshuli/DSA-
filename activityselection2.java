0d2cb5d7a21d6156d9c701a860af6d75eca75995
Ayush-Anshuli <109850987+Ayush-Anshuli@users.noreply.github.com>
2023-03-04 17:25:23 +0530

    Add files via upload

---
+++ activityselection2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class activityselection2 {
+    public static void main(String[] args) {
+        int start[]={1,3,0,5,8,5};
+        int end[]={2,4,6,7,9,9};
+
+        int act[][]=new int[start.length][3];
+        for(int i=0;i<start.length;i++){
+            act[i][0]=i;
+            act[i][1]=start[i];
+            act[i][2]=end[i];
+        }
+
+        Arrays.sort(act,Comparator.comparing(o->o[2]));
+
+        int maxAct=0;
+        ArrayList<Integer> ans=new ArrayList<>();
+        maxAct=1;
+        ans.add(act[0][0]);
+        int endtime=act[0][2];
+
+        for(int i=1;i<end.length;i++){
+            if(act[i][1]>=endtime){
+                // Activity select
+                maxAct++;
+                ans.add(act[i][0]);
+                endtime=act[i][2];
+            }
+        }
+        System.out.println("Max Activity = "+maxAct);
+        for(int i=0;i<ans.size();i++){
+            System.out.print("A"+ans.get(i)+" ");
+        }
+        System.out.println();
+    }
+}
